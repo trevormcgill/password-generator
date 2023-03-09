074fb98f488cb8d570129b9817450090d1e926b5
Trevor McGill <trevor.mcgill@icloud.com>
2023-03-09 15:00:05 -0600

    removed indentation of some comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,7 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-  // Define character sets to use in password
+// Define character sets to use in password
 var characterOptionsLowercase = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
 var characterOptionsUppercase = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z";
 var numberOptions = "0 1 2 3 4 5 6 7 8 9";
@@ -25,7 +25,7 @@ var charUpper = characterOptionsUppercase.split(' ');
 var specialChars = symbolOptionsList.split(' ');
 var numericChars = numberOptions.split(' ');
 
-  // Function to generate password
+// Function to generate password
 function generatePassword() {
   var passwordLength = prompt("How many characters would you like your password to contain? (Enter a number between 8 and 128)");
   passwordLength = parseInt(passwordLength);
@@ -44,7 +44,7 @@ function generatePassword() {
   
   var allCharacters = [];
   
-  // Prompt user to build character set based on user options
+// Prompt user to build character set based on user options
   var includeLowercase = confirm("Click OK to include lowercase characters");
   if (includeLowercase) {
     allCharacters = allCharacters.concat(charLower);
@@ -65,7 +65,7 @@ function generatePassword() {
     allCharacters = allCharacters.concat(numericChars);
   }
 
-  // Setting an empty string so the random characters get added to it
+// Setting an empty string so the random characters get added to it
   var finalPass = "";
 
   for (var i = 0; i < passwordLength; i++) {
